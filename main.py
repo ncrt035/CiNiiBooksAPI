cd12a8b24c6f56c180bfbd884f9f6e3950f4f282
Tetsufumi Takeshita <takeshita.tetsufumi.44z@kyoto-u.jp>
2018-08-10 16:23:05 +0900

    First commit

---
+++ main.py
@@ -0,0 +1,31 @@
+#main.py
+#coding:utf-8
+
+import requests
+import csv
+
+url = "http://ci.nii.ac.jp/books/opensearch/search?"
+csvFile = open("sample.csv", encoding="utf-8")
+
+csvDict = csv.DictReader(csvFile, delimiter=",")#csvを辞書型で読み込み
+
+result = open("result.txt","a",encoding="utf-8")#結果書き出し用ファイル準備
+
+for row in csvDict:
+    author = row["author"]
+    title = row["title"]
+
+    query = {"format":"json",
+             "author":author,
+             "title":title,
+             "lang":"jpn",
+             "count":10}
+    res = requests.get(url,params=query)
+
+    if res.json()["@graph"][0]["opensearch:itemsPerPage"] != "0":#検索結果が0件でない場合
+        for i in res.json()["@graph"][0]["items"]:
+            str = i["dc:creator"] + ":" + i["title"] + "\n"
+
+            result.write(str)
+
+result.close()
