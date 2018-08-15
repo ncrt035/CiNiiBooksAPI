155c296ccb6d987b20e27365a5be0e3fdd429ecd
Tetsufumi Takeshita <takeshita.tetsufumi.44z@kyoto-u.jp>
2018-08-15 10:44:05 +0900

    Add else sentence etc.

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ import csv
 url = "http://ci.nii.ac.jp/books/opensearch/search?"
 csvFile = open("sample.csv", encoding="utf-8")
 
-csvDict = csv.DictReader(csvFile, delimiter=",")#csvを辞書型で読み込み
+csvDict = csv.DictReader(csvFile, delimiter=",", quotechar='"')#csvを辞書型で読み込み
 
 result = open("result.txt","a",encoding="utf-8")#結果書き出し用ファイル準備
 
 for row in csvDict:
     author = row["author"]
     title = row["title"]
+    str = author + ", " + title + " 邦訳："
 
     query = {"format":"json",
              "author":author,
@@ -24,8 +25,11 @@ for row in csvDict:
 
     if res.json()["@graph"][0]["opensearch:itemsPerPage"] != "0":#検索結果が0件でない場合
         for i in res.json()["@graph"][0]["items"]:
-            str = i["dc:creator"] + ":" + i["title"] + "\n"
+            str += i["dc:creator"] + "，" + i["title"] + "\n"
 
             result.write(str)
 
+    else:
+        str += "該当なし"
+        result.write(str)
 result.close()
